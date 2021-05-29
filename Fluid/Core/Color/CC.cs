5d52b5a411ce4749b3e130a2ccdd3b14ce6955f6
CXCubeHD <cxcubehd@gmail.com>
2021-05-29 18:04:48 +0200

    polished the ChatColor names

--- Fluid/Core/Color/CC.cs
+++ Fluid/Core/Color/CC.cs
@@ -9,23 +9,23 @@ namespace Fluid.Core
         /// </summary>
         public static string Red = "§c";
         public static string Black = "§0";
-        public static string darkBlue = "§1";
-        public static string darkGreen = "§2";
-        public static string darkAqua = "§3";
-        public static string darkRed = "§4";
-        public static string darkPurple = "§5";
+        public static string DarkBlue = "§1";
+        public static string DarkGreen = "§2";
+        public static string DarkAqua = "§3";
+        public static string DarkRed = "§4";
+        public static string DarkPurple = "§5";
         public static string Gold = "§6";
         public static string Gray = "§7";
         public static string darkGray = "§8";
         public static string Blue = "§9";
         public static string Green = "§a";
         public static string Aqua = "§b";
-        public static string lightPurple = "§d";
+        public static string LightPurple = "§d";
         public static string Yellow = "§e";
         public static string White = "§f";
-        public static string chatObfuscated = "§k";
-        public static string chatBold = "§l";
-        public static string chatItalic = "§o";
-        public static string chatReset = "§r";
+        public static string Obfuscated = "§k";
+        public static string Bold = "§l";
+        public static string Italic = "§o";
+        public static string Reset = "§r";
     }
 }
