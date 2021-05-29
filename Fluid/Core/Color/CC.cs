2736b431ddfde145d8cc2dd5e73d776a742ff76a
CXCubeHD <cxcubehd@gmail.com>
2021-05-29 18:08:44 +0200

    Added descriptions to all ChatColors

--- Fluid/Core/Color/CC.cs
+++ Fluid/Core/Color/CC.cs
@@ -10,38 +10,98 @@ namespace Fluid.Core
         public static string Red = "§c";
         
         /// <summary>
-        /// Red §c
+        /// Black §0
         /// </summary>
         public static string Black = "§0";
         
         /// <summary>
-        /// Red §c
+        /// DarkBlue §1
         /// </summary>
         public static string DarkBlue = "§1";
         
         /// <summary>
-        /// Red §c
+        /// DarkGreen §2
         /// </summary>
         public static string DarkGreen = "§2";
         
         /// <summary>
-        /// Red §c
+        /// DarkAqua §3
         /// </summary>
         public static string DarkAqua = "§3";
+        
+        /// <summary>
+        /// DarkRed §4
+        /// </summary>
         public static string DarkRed = "§4";
+        
+        /// <summary>
+        /// DarkPurple §5
+        /// </summary>
         public static string DarkPurple = "§5";
+        
+        /// <summary>
+        /// Gold §6
+        /// </summary>
         public static string Gold = "§6";
+        
+        /// <summary>
+        /// Gray §7
+        /// </summary>
         public static string Gray = "§7";
-        public static string darkGray = "§8";
+        
+        /// <summary>
+        /// DarkGray §8
+        /// </summary>
+        public static string DarkGray = "§8";
+        
+        /// <summary>
+        /// Blue §9
+        /// </summary>
         public static string Blue = "§9";
+        
+        /// <summary>
+        /// Green §a
+        /// </summary>
         public static string Green = "§a";
+        
+        /// <summary>
+        /// Aqua §b
+        /// </summary>
         public static string Aqua = "§b";
+        
+        /// <summary>
+        /// LightPurple §d
+        /// </summary>
         public static string LightPurple = "§d";
+        
+        /// <summary>
+        /// Yellow §e
+        /// </summary>
         public static string Yellow = "§e";
+        
+        /// <summary>
+        /// White §f
+        /// </summary>
         public static string White = "§f";
+        
+        /// <summary>
+        /// Obfuscated §k
+        /// </summary>
         public static string Obfuscated = "§k";
+        
+        /// <summary>
+        /// Bold §l
+        /// </summary>
         public static string Bold = "§l";
+        
+        /// <summary>
+        /// Italic §o
+        /// </summary>
         public static string Italic = "§o";
+        
+        /// <summary>
+        /// Reset §r
+        /// </summary>
         public static string Reset = "§r";
     }
 }
