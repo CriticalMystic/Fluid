3c7a53438a9e27d1b532b69cad062e8dd7021908
Verox001 <altergeist987@gmail.com>
2021-05-29 15:59:31 +0200

    Begin with OnlinePlayer

--- Fluid/Core/Entity/Entities/OnlinePlayer.cs
+++ Fluid/Core/Entity/Entities/OnlinePlayer.cs
@@ -3,5 +3,12 @@ namespace Fluid.Core
     public class OnlinePlayer : Player
     {
         public override bool Is<T>() => typeof(T) == typeof(OnlinePlayer);
+
+        private int gamemode = 1;
+
+        public int getGamemode()
+        {
+            return this.gamemode;
+        }
     }
 }
